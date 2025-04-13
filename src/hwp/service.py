3169659a260ed1c0f7c52e7ccef7a9d960c25cfc
Ruzzy77 <19502719+Ruzzy77@users.noreply.github.com>
2025-04-13 14:14:00 +0900

    Register FilePathCheckModule

--- src/hwp/service.py
+++ src/hwp/service.py
@@ -1,3 +1,4 @@
+import winreg
 from contextlib import contextmanager
 from pathlib import Path
 from typing import Any, Dict
@@ -25,12 +26,86 @@ def hwp_context(visible: bool = False):
     Returns:
         Hwp: HWP 객체
     """
-    hwp = Hwp(new=True, visible=visible)
+    hwp = Hwp(new=True, visible=visible, register_module=False)
+    print("Hwp version:", hwp.Version)
+
+    register_security_module(hwp, "resource/FilePathCheckerModule.dll")
+
     try:
         yield hwp
     finally:
         hwp.clear()
         hwp.quit()
+
+
+def register_security_module(
+    hwp: Hwp,
+    dll_filepath: str = "FilePathCheckerModule.dll",
+) -> bool:
+    """
+    한글 HwpAutomation에 보안모듈 DLL을 레지스트리에 등록하고, HWP 객체에 등록합니다.
+    레지스트리 등록을 위한 관리자 권한이 필요합니다.
+    레지스트리에 등록된 DLL은 HWP 객체에 보안모듈로 등록됩니다.
+
+    레지스트리 값 이름을 'FilePathCheckerModule' 이외 이름으로 레지스트리에 등록할 경우
+    pyhwpx 패키지 내부에서 `FileNotFoundError` 문제가 발생하여,
+    DLL 이름은 'FilePathCheckerModule.dll'로 고정합니다.
+        https://github.com/martiniifun/pyhwpx/issues/8
+
+    Args:
+        hwp (Hwp): pyhwpx로 생성된 Hwp 객체
+        dll_filepath (str): 등록할 보안모듈 DLL 파일 경로
+            (상대경로일 경우, 현재 작업 디렉토리 기준으로 해석됨)
+
+    Returns:
+        bool: 보안모듈 등록 결과
+    """
+
+    dll_path = Path(dll_filepath).resolve()
+    if not dll_path.exists():
+        raise FileNotFoundError(f"DLL 파일이 존재하지 않습니다: {dll_path}")
+
+    module_name = dll_path.stem  # 'FilePathCheckerModuleExample'
+    reg_path = r"Software\HNC\HwpAutomation\Modules"
+
+    # 1-1. 레지스트리 문자열 값(FilePathCheckerModuleExample) 존재 확인
+    try:
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, reg_path) as key:
+            try:
+                value, _ = winreg.QueryValueEx(key, module_name)
+                print(f"[✔] 레지스트리에 보안모듈 등록됨: {module_name} → {value}")
+            except FileNotFoundError:
+                print("[✖] 레지스트리에 보안모듈이 등록되어 있지 않습니다. 새로 등록합니다.")
+
+                with winreg.CreateKey(winreg.HKEY_CURRENT_USER, reg_path) as key:
+                    winreg.SetValueEx(key, module_name, 0, winreg.REG_SZ, str(dll_path))
+                    print(f"[✔] 레지스트리에 보안모듈 등록 완료: {module_name} → {dll_path}")
+    except PermissionError:
+        print("[✖] 레지스트리 접근 권한이 없습니다. 관리자 권한으로 실행하세요.")
+        return False
+
+    # self.hwp = win32.gencache.EnsureDispatch("HWPFrame.HwpObject")
+    # self.hwp.XHwpWindows.Active_XHwpWindow.Visible = visible
+
+    # 2. HWP 객체에 보안모듈 등록 (pyhwpx 활용)
+    result = hwp.RegisterModule("FilePathCheckDLL", module_name)
+
+    if not result:
+        print(f"[✖] HWP 객체에 보안모듈 활성화 실패: {module_name}")
+        print(
+            rf"""
+            아래 링크를 참조하여 보안모듈을 레지스트리에 수동으로 등록하세요. (관리자 권한 필요)
+                https://developer.hancom.com/hwpautomation
+
+            DLL 파일을 'C:\\Program Files (x86)\\HNC\\HwpAutomation\\Modules' 폴더(또는 원하는 위치)에 복사한 후,
+            레지스트리 편집기를 열고 아래 경로로 이동하여 DLL 경로를 문자열 값으로 등록하세요. (새로 만들기 → 문자열 값)
+                컴퓨터\HKEY_CURRENT_USER\Software\HNC\HwpAutomation\Modules
+                - 값 이름: {module_name}
+                - 값 데이터: {dll_path}
+            """
+        )
+
+    return result
 
 
 def process_documents(
@@ -52,8 +127,6 @@ def process_documents(
         None
     """
     with hwp_context(visible=False) as hwp:
-        print("Hwp version:", hwp.Version)
-
         # 템플릿 열기
         open_template(hwp, template_path)
 
